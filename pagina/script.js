451a9a910ae6c7c529dda6f17603aba344a48b28
Christian Nagy <cnagy@technisys.com>
2020-09-30 10:38:45 -0300

    agrego alerta js

---
+++ pagina/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () =>{
+
+
+    document.querySelector("#alerta").addEventListener("click" , ()=>{
+
+        alert("soy una alerta");
+
+    })
+
+
+
+
+});
