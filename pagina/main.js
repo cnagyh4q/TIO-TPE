51d39ded3399ea57b787a80870ad77d519464858
franf16 <franchinif16@gmail.com>
2020-10-13 18:08:31 -0300

    agrego partial render (falta mostrar la home cuando se carga la pagina)

---
+++ pagina/main.js
@@ -0,0 +1,16 @@
+const TEMPLATES_URL = './templates/';
+let content; // div donde se muestra el contenido traido con partial render
+
+async function load(templateName) {
+    try {
+        let html = await fetch(TEMPLATES_URL + templateName + '.html').then(r => r.text());
+        content.innerHTML = html;
+    }
+    catch (e) {
+        content.innerHTML = e.message;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    content = document.querySelector('#content');
+});
